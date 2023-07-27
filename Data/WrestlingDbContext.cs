b08d87b1eee1f8d5c17001953a4d42e4b0a996a3
Edwin Torres <me@edwintorres.dev>
2023-07-26 20:08:48 -0400

    Update WrestlingDbContext.cs

--- Data/WrestlingDbContext.cs
+++ Data/WrestlingDbContext.cs
@@ -6,9 +6,9 @@ namespace WrestlingMVC.Data
 	{
 		public WrestlingDbContext(DbContextOptions<WrestlingDbContext> options) : base(options) { }
 
-		public DbSet<Promotion> Promotions { get; set; }
-		public DbSet<Championship> Championships { get; set; }
-		public DbSet<Wrestler> Wrestlers { get; set; }
-		public DbSet<Reign> Reigns { get; set; }
+		public DbSet<Promotion> Promotions { get; set; } = null!;
+		public DbSet<Championship> Championships { get; set; } = null!;
+		public DbSet<Wrestler> Wrestlers { get; set; } = null!;
+		public DbSet<Reign> Reigns { get; set; } = null!;
 	}
 }
