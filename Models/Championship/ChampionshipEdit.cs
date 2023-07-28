65cc8724605c85a9b9eba576ca305e2d70bd640d
Edwin Torres <me@edwintorres.dev>
2023-07-28 08:25:21 -0400

    Lots missing but functional

--- Models/Championship/ChampionshipEdit.cs
+++ Models/Championship/ChampionshipEdit.cs
@@ -1,28 +1,36 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace WrestlingMVC.Models.Championship;
-
-public class ChampionshipEdit
+namespace WrestlingMVC.Models.Championship
 {
-	[Required]
-	public int Id { get; set; }
+	public class ChampionshipEdit
+	{
+		[Required]
+		public int PromotionId { get; set; }
 
-	[Required]
-	[StringLength(100)]
-	public string? Name { get; set; }
+		[Required]
+		public int Id { get; set; }
 
-	[StringLength(500)]
-	public string? Image { get; set; }
+		[Required]
+		[StringLength(100)]
+		public string? Name { get; set; }
 
-	[Required]
-	public bool Status { get; set; }
+		[StringLength(500)]
+		public string? Image { get; set; }
 
-	[Required]
-	[Display(Name = "Established")]
-	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-	public DateTime? Established { get; set; }
+		[Required]
+		public bool Status { get; set; }
 
-	[Display(Name = "Retired")]
-	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-	public DateTime? Retired { get; set; }
+		[Required]
+		[Display(Name = "Established")]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime? Established { get; set; }
+
+		[Display(Name = "Retired")]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime? Retired { get; set; }
+
+		// Add a property to hold the list of promotions
+		public IEnumerable<SelectListItem> Promotions { get; set; } = new List<SelectListItem>();
+	}
 }
